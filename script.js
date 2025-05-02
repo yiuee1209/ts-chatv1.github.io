2ac7e564c72bad0763632f060da4e550be9c82a7
yiuee1209 <117779881+yiuee1209@users.noreply.github.com>
2025-05-02 12:25:17 +0800

    Update script.js

--- script.js
+++ script.js
@@ -68,6 +68,13 @@ document.addEventListener('DOMContentLoaded',()=>{
                 recordButton.textContent = "🎤 開始錄音";
                 console.log("錄音已停止。");
                 // 等待結果回傳 (下一步處理)
+                
+
+                if (Recorder.websocket && Recorder.websocket.readyState === WebSocket.OPEN) {
+                    Recorder.websocket.send("EOS");
+                    console.log("已發送 EOS 信號。");
+                }
+
 
     
             } catch (error) {
