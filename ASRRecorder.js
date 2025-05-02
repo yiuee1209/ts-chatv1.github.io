bc9fad3c1bc38b747b9a91308cf01b01ef9799d9
yiuee1209 <117779881+yiuee1209@users.noreply.github.com>
2025-05-02 10:21:25 +0800

    Update ASRRecorder.js

--- ASRRecorder.js
+++ ASRRecorder.js
@@ -358,7 +358,7 @@ class ASRRecorder {
     await this.login();
     await this.getWebsocketAccessInfo();
 
-    this.setupParserConfig(parserUrl);
+    //this.setupParserConfig(parserUrl);
     await this.parser.connect();
 
     await this.connectWebsocket();
